370b02e904bc564431c881674a3939a1a9279903
hisato-kawaji <hisato.kawaji@gmail.com>
2013-10-29 05:27:15 +0900

    initial commit for web app and core code

---
+++ ena-web/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnaWeb::Application.config.secret_key_base = 'e1ecd9b5bf0c67c64577d105d870579355a835a2c1c8b0023443f99b7473e742ecd907537b589390fabba010f0ec16aae2cc28cd59f2ed881ad5c92a36728190'
