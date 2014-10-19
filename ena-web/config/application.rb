b196a8439b8bc53325a2fe212da88c2f15c4e612
hisato-kawaji <hisato.kawaji@gmail.com>
2014-10-20 01:26:05 +0900

    fix twitter boot strap

--- ena-web/config/application.rb
+++ ena-web/config/application.rb
@@ -11,6 +11,10 @@ module EnaWeb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+	config.time_zone = 'Tokyo'
+	config.generators do |g|
+	  g.template_engine :haml
+	end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
